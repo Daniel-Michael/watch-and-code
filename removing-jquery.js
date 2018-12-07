24628f8e2a09355c0b1077190b65351ae3e27ef2
Dan Michael <danmikhael@gmail.com>
2018-12-07 11:41:19 +1100

    removing `edit` method

--- removing-jquery.js
+++ removing-jquery.js
@@ -1,38 +1,50 @@
 // TodoMVC App `create` method With jQuery
 
 create: function (e) {
-			var $input = $(e.target);
-			var val = $input.val().trim();
-
-			if (e.which !== ENTER_KEY || !val) {
-				return;
-			}
-
-			this.todos.push({
-				id: util.uuid(),
-				title: val,
-				completed: false
-			});
-
-			$input.val('');
-
-			this.render();
+  var $input = $(e.target);
+  var val = $input.val().trim();
+    if (e.which !== ENTER_KEY || !val) {
+    return;
+    }
+this.todos.push({
+  id: util.uuid(),
+  title: val,
+  completed: false
+});
+$input.val('');
+this.render();
 		},
     
   // `create` method without jQuery
   
-  create: function (e) {  
-      var input = e.target; 
-      var val = input.value;        
-        if (e.which !== ENTER_KEY || !input) {              
-          return;                               
-        }			
-      this.todos.push({                         				
-        id: util.uuid(),                        				 
-        title: input.value,                              
-				completed: false  
-			});			
+create: function (e) {  
+  var input = e.target; 
+  var val = input.value;        
+    if (e.which !== ENTER_KEY || !input) {              
+    return;                               
+    }			
+  this.todos.push({                         				
+  id: util.uuid(),                        				 
+  title: input.value,                              
+  completed: false  
+});			
       
       input.value = '';   			
       this.render();        
-		},
+},
+			
+// TodoMVC App `edit` method With jQuery
+			
+// `edit` method with jQuery
+edit: function (e) {
+  var $input = $(e.target).closest('li').addClass('editing').find('.edit');
+  $input.val($input.val()).focus();
+},
+
+// `edit` method in vanilla JS
+	
+edit: function (e) { 			          
+  var editLi = (e.target).closest('li'); 
+  editLi.classList.add('editing');        
+  editLi.querySelector('.edit').focus();                                          
+},
